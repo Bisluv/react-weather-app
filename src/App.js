2aebeb8e365893355743f636312cd281d72ad562
Akangbe Kafilat Bisola <ojuawobisola25@gmail.com>
2023-12-23 15:24:52 +0100

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 function App() {
   return (
-    <div className="App">Weather App </div>
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Akangbe kafilat and is 
+        <a href="https://github.com/Bisluv/reat-weather-app"
+        target="_blank">open-sourced on Github</a>
+      </footer>
+     </div>
+
   );
 }
 
