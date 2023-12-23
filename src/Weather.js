f02758b40d81c5d6f05b2dae4f089446409cc6d2
Akangbe Kafilat Bisola <ojuawobisola25@gmail.com>
2023-12-23 22:53:52 +0100

    Add snapshot

--- src/Weather.js
+++ src/Weather.js
@@ -3,18 +3,28 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
       <input
         type="search"
-        placsholder="Enter a City..."
+        placeholder="Enter a City..."
         className="form-control"
       />
+      </div>
+      <div className="col-3">
+      <input type="submit" value="search" className="btn btn-primary"/>
+      
+      </div>
+    </div>
+      </form>
       <h1>Newyork</h1>
       <ul>
         <li>Saturday 09:41</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div class="row">
-        <div class="col-6">
+      <div className="row">
+        <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Mostly Cloudy"
@@ -28,7 +38,7 @@ export default function Weather() {
             <li>Wind 13km/h%</li>
           </ul>
         </div>
-      </div>
+    </div>
     </div>
   );
 }
